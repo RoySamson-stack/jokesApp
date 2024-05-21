126060fe80c98b0cdf289ee08f9768726f16689c
RoySamson-stack <you@example.com>
2024-05-21 15:00:01 +0300

    changing the data on the home and privacy pages

--- JokesWebApp/obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
+++ JokesWebApp/obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f387f743a9b5193c7297de801b67d98fd487bb6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0f1bc4870bf332f795dbfa1131aadf2eb993e8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
