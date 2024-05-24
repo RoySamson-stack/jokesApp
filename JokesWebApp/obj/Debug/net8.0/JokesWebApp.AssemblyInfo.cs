5c3ef0fed48e39aec2cdedcafd1575701c40f268
RoySamson-stack <you@example.com>
2024-05-24 17:06:03 +0300

    adding the sql server to the code

--- JokesWebApp/obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
+++ JokesWebApp/obj/Debug/net8.0/JokesWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126060fe80c98b0cdf289ee08f9768726f16689c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+549f992d172d0995fe9d26a663013337a2fccd61")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokesWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
